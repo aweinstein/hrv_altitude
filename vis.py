1c5080e66cc1d6c38de5d6033f88697990209175
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2016-08-17 05:34:15 -0300

    Add plot pairs function.

--- vis.py
+++ vis.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 sns.set(style="ticks")
 
 def plot_RR(hrv):
@@ -13,3 +14,24 @@ def plot_HR(hrv):
     grid.map(plt.plot, 'time', 'HR')
     grid.set_axis_labels('time [s]', 'Heart rate [bpm]')
     grid.add_legend()
+
+def pair_plot(metrics):
+
+    cols = [#'meanRR',
+            #'meanHR',
+            'SDNN',
+            'RMSSD',
+            #'peak_VLF', 'peak_LF', 'peak_HF',
+            #'power_VLF', 'power_LF', 'power_HF',
+            'peak_HF',
+            'power_LFHF',
+            #'pcpower_VLF', 'pcpower_LF', 'pcpower_HF',
+            #'nupower_LF', 'nupower_HF'
+    ]
+    sns.pairplot(metrics, hue='height', vars=cols)
+
+if __name__ == '__main__':
+    metrics = pd.read_pickle('dfs/metrics.pkl')
+    plt.close('all')
+    pair_plot(metrics.reset_index())
+    plt.show()
