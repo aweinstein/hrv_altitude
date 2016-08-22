ee07de4ac9f6908f73c5f3904ae17a10f88285b3
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2016-08-22 12:32:48 -0300

    Add strip plot.

--- vis.py
+++ vis.py
@@ -1,16 +1,19 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 sns.set(style="ticks")
 
 def plot_RR(hrv):
-    grid = sns.FacetGrid(hrv, col='subject', hue='height', col_wrap=4, legend_out=False)
+    grid = sns.FacetGrid(hrv, col='subject', hue='height', col_wrap=4,
+                         legend_out=False)
     grid.map(plt.plot, 'time', 'RR')
     grid.set_axis_labels('time [s]', 'RR interval [ms]')
     grid.add_legend()
 
 def plot_HR(hrv):
-    grid = sns.FacetGrid(hrv, col='subject', hue='height', col_wrap=4, legend_out=False)
+    grid = sns.FacetGrid(hrv, col='subject', hue='height', col_wrap=4,
+                         legend_out=False)
     grid.map(plt.plot, 'time', 'HR')
     grid.set_axis_labels('time [s]', 'Heart rate [bpm]')
     grid.add_legend()
@@ -30,8 +33,23 @@ def pair_plot(metrics):
     ]
     sns.pairplot(metrics, hue='height', vars=cols)
 
+def violin(metrics, var):
+    # ax = sns.violinplot(x='height', y=var, data=metrics,
+    #                     inner=None, color=".8")
+    ax = sns.stripplot(x='height', y=var, data=metrics, jitter=0.05)
+    left = ax.collections[0].get_offsets()
+    right = ax.collections[1].get_offsets()
+    x = np.vstack((right[:,0], left[:,0]))
+    y = np.vstack((right[:,1], left[:,1]))
+    plt.plot(x, y, lw=0.3, c='k')
+
+    return ax
+
+
 if __name__ == '__main__':
     metrics = pd.read_pickle('dfs/metrics.pkl')
     plt.close('all')
-    pair_plot(metrics.reset_index())
+
+    # pair_plot(metrics.reset_index())
+    ax = violin(metrics.reset_index(), 'power_LFHF')
     plt.show()
